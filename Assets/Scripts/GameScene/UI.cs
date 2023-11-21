b9d82a90c74653372c6a3358945a9a04048cebf6
yuchan <rladbcks56@naver.com>
2023-11-21 19:47:08 +0900

    HP, trail
    및 스프라이트 리셋

--- Assets/Scripts/GameScene/UI.cs
+++ Assets/Scripts/GameScene/UI.cs
@@ -6,39 +6,32 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour //UI 전부를 다루는 스크립트
 {
-    public GameObject player;
-    public GameObject healthBarObject;
-    public GameObject maxHealthTextObject;
-    public GameObject healthTextObject;
-    public GameObject massTextObject;
-    public GameObject weightTextObject;
-    private PlayerProperties playerProperties;
-    private Slider healthBar;
-    private TextMeshProUGUI maxHealthText;
-    private TextMeshProUGUI healthText;
-    private TextMeshProUGUI massText;
-    private TextMeshProUGUI weightText;
+    public Player player;
+    public PlayerProperties playerProperties;
+    public TextMeshProUGUI maxHealthText;
+    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI massText;
+    public TextMeshProUGUI weightText;
+    public Image maxHealthBar;
+    public Image healthBar;
 
     void Start()
     {
+        maxHealthBar = maxHealthBar.GetComponent<Image>();
+        healthBar = healthBar.GetComponent<Image>();
         playerProperties = player.GetComponent<PlayerProperties>();
-        healthBar = healthBarObject.GetComponent<Slider>();
-        maxHealthText = maxHealthTextObject.GetComponent<TextMeshProUGUI>();
-        healthText = healthTextObject.GetComponent<TextMeshProUGUI>();
-        massText = massTextObject.GetComponent<TextMeshProUGUI>();
-        weightText = weightTextObject.GetComponent<TextMeshProUGUI>();
+        maxHealthText = maxHealthText.GetComponent<TextMeshProUGUI>();
+        healthText = healthText.GetComponent<TextMeshProUGUI>();
+        massText = massText.GetComponent<TextMeshProUGUI>();
+        weightText = weightText.GetComponent<TextMeshProUGUI>();
     }
 
     
     void Update()
     {
-        //0이 min, 1이 max
-        healthBar.value = playerProperties.health / playerProperties.maxHealth;
-        //0이 되도 조금 남기 때문에, 아예 비활성화
-        if (healthBar.value <= 0)
-            healthBar.transform.Find("Fill Area").gameObject.SetActive(false);
-        else
-            healthBar.transform.Find("Fill Area").gameObject.SetActive(true);
+
+        maxHealthBar.rectTransform.sizeDelta = new Vector2(playerProperties.maxHealth, 13);
+        healthBar.rectTransform.sizeDelta = new Vector2(playerProperties.health, 13);
 
         //float값 뒤의 소수점 자르기
         maxHealthText.text = ((int)playerProperties.maxHealth).ToString();
