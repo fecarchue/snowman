020bdc1eba335bf754e9c425024e7188c7b0b5f9
mingi <chamingy1@gmail.com>
2024-05-03 05:13:13 +0900

    인벤토리 고정 해상도

--- Assets/Scripts/ObjectInfomation.cs
+++ Assets/Scripts/ObjectInfomation.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ObjectInfomation : MonoBehaviour
 {
-    public GameObject InfoWindow, closebutton, SampleStar, SampleRock;
-
+    public GameObject InfoWindow;
+    public GameObject stonePrefab, treePrefab;
     public void MakeObject(List<int> objects)
     {
         //있던 오브젝트들 제거
@@ -20,17 +21,11 @@ public class ObjectInfomation : MonoBehaviour
             //샘플 오브젝트 복사해서 생성
             if (obj / 10000 == 21)
             {
-                GameObject newObject = Instantiate(SampleStar);
-                newObject.transform.SetParent(InfoWindow.transform);
-                RectTransform rectTransform = newObject.GetComponent<RectTransform>();
-                rectTransform.localScale = new Vector3(5f, 5f, 1f);
+                Instantiate(stonePrefab, this.transform);
             }
             else if(obj == 654321)
             {
-                GameObject newObject = Instantiate(SampleRock);
-                newObject.transform.SetParent(InfoWindow.transform);
-                RectTransform rectTransform = newObject.GetComponent<RectTransform>();
-                rectTransform.localScale = new Vector3(1f, 1f, 1f);
+                Instantiate(treePrefab, this.transform);
             }
         }
     }
